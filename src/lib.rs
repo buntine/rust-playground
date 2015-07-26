e867bd5e2e024d7e2753fc17713800e9759cfd39
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-07-26 18:37:35 +1000

    Added imperative implementation of #2

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,28 @@
 /// Find the sum of all the multiples of 3 or 5 below 1000.
 pub fn euler1(bound: u32) -> u32 {
     (1..bound)
-    .filter(|n| n % 3 == 0 || n % 5 == 0)
+    .filter(|&n| n % 3 == 0 || n % 5 == 0)
     .fold(0, |s, n| s + n)
+}
+
+
+/// By considering the terms in the Fibonacci sequence whose values do not exceed four million,
+/// find the sum of the even-valued terms.
+pub fn euler2_imperative(bound: u32) -> u32 {
+    let mut x = 1;
+    let mut y = 2;
+    let mut z;
+    let mut sum= 0;
+
+    while y < bound {
+        if y % 2 == 0 {
+            sum += y;
+        }
+
+        z = x + y;
+        x = y;
+        y = z;
+    }
+
+    sum
 }
--- tests/lib.rs
+++ tests/lib.rs
@@ -4,5 +4,11 @@ extern crate playground;
 fn euler1() {
     assert_eq!(23, playground::euler1(10));
     assert_eq!(78, playground::euler1(20));
-    assert_eq!(233_168, playground::euler1(1000));
+    assert_eq!(233_168, playground::euler1(1_000));
+}
+
+#[test]
+fn euler2_imperative() {
+    assert_eq!(44, playground::euler2_imperative(100));
+    assert_eq!(4613732, playground::euler2_imperative(4_000_000));
 }
